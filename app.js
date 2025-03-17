fef8e00380f693e069d06afa4095807cddf30ad4
Poppy Mecha <hugooo.aburto.tobar@gmail.com>
2025-03-17 17:10:58 -0300

    Finaliza puntos necesarios app.js

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let listaAmigoSecreto = [];
+let listaAmigos = [];
 let nombreSorteado = [];
-let cantidadDeAmigos = listaAmigoSecreto.length;
 
 
 function agregarAmigo() {
-    let nombreDeAmigo = document.querySelector('#amigo').value;
-    
-    if (nombreDeAmigo == ''){
+    let nombreAmigo = document.querySelector('#amigo');
+    let amigo = nombreAmigo.value;
+
+    nombreAmigo.focus();
+    if (amigo == ''){
         alert('Ingrese un nombre válido');
     } else {
-        listaAmigoSecreto.push(nombreDeAmigo);
-        limpiarCaja();
+        listaAmigos.push(amigo);
+        nombreAmigo.value = "";
+        mostrarListaAmigos();
+        
     }
-    console.log(listaAmigoSecreto);
+    console.log(listaAmigos);
 }
 
 
-function limpiarCaja() {
-    document.getElementById('amigo').value ='';
+function mostrarListaAmigos() {
+    let listaImpresa = document.getElementById("listaAmigos");
+    listaImpresa.innerHTML = "";
+    for (let i = 0; i < listaAmigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = listaAmigos[i];
+        listaImpresa.appendChild(li);
+    }
+    return;
 }
+
 
 function sortearAmigo() {
-    let amigoSorteado = parseInt(Math.floor(Math.random()*cantidadDeAmigos));
+    let amigoSorteado = listaAmigos[Math.floor(Math.random()*listaAmigos.length)];
+    let resultado = document.getElementById("resultado");
+    if (listaAmigos.length == 0){
+        alert('Lista de amigos vacía, ingrese al menos un nombre');
+        return;
+    } else {
+        resultado.innerHTML = `Tu amigo secreto es ${amigoSorteado}!!!`
+    }
 }
